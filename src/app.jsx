2b910f38590a6b2f67849f94dd5ad6c072db9526
amin kiyani <m.zobedei@gmai.com>
2024-02-29 15:22:55 +0100

    new React app

---
+++ src/app.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Note from "./note"
+import Heading from "./Header";
+import Footer from "./Footer";
+
+function App(){
+    return <div><Heading/>
+    <Note/>
+    <Footer/>
+
+    </div>
+}
+
+export default App;
+
---
+++ src/note.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Note(){
+    return <div className="note">
+        <h1>This ss Note Title</h1>
+        <p>This is Note Conten</p>
+    </div>
+}
+
+export default Note;
